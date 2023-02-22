9280ee2f38ce10b871e93d8692a8169549a314b1
Elijah Onchwari <elijah.ndege.onchwari@gmail.com>
2023-02-22 17:34:47 +0300

    feat(root) forked website

---
+++ src/components/icons/loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const IconLoader = () => (
+  <svg id="logo" viewBox="0 0 134 157" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <title>logo</title>
+  <g id="EO:-_website_logo" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <path d="M67.727334,5.60918182 L129.675154,42.2010811 L129.675154,115.564436 L67.727334,152.156335 L5.77951359,115.564436 L5.77951359,42.2010811 L67.727334,5.60918182 Z" id="Polygon" stroke="#0097A7" strokeWidth="9" fill="#03122B"></path>
+      <text id="B" fontFamily="Verdana" fontSize="85" fontWeight="normal" fill="#0097A7">
+          <tspan x="41" y="100">e</tspan>
+      </text>
+  </g>
+</svg>
+);
+
+export default IconLoader;
